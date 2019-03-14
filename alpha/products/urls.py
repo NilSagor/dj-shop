1926e045b7404525fb8aca87c3315243e30637f2
NilSagor <masum1201@gmail.com>
2019-03-14 09:02:15 +0800

    product list view and templates added

--- alpha/products/urls.py
+++ alpha/products/urls.py
@@ -14,26 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
 
 
 
-from django.conf import settings
-from django.conf.urls.static import static
+
 
 from . import views
 
 urlpatterns = [
-	path('', views.HomePageView.as_view(), name = 'home'),
-	path('contact/', views.ContactPageView.as_view(), name = 'contact'),
-	path('login/', views.loginPageView, name = 'login'),
-	path('register/', views.RegisterPageView, name = 'register'),
-	path('products/', include('products.urls')),
-    path('admin/', admin.site.urls),
+	path('', views.ProductListView.as_view(), name = 'product-list'),
+	
 ]
 
 
 
-urlpatterns += (static(settings.STATIC_URL, document_root=settings.STATIC_ROOT))
 
-urlpatterns += (static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT))
--- alpha/products/views.py
+++ alpha/products/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.view.generic import ListView
+
+from .models import Product
+
+
 
 # Create your views here.
+class ProductListView(ListView):
+	model = Product
+	template_name = 'products/product_list.html'
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductListView, self).get_context_data(*args, **kwargs)
+		print(context)
+		return context
