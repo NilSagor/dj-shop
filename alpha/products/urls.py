a89d1e731c41c2b08a8a8b51fed60569ce752ff0
NilSagor <masum1201@gmail.com>
2019-03-14 09:18:35 +0800

    detail view added

--- alpha/products/urls.py
+++ alpha/products/urls.py
@@ -24,6 +24,7 @@ from . import views
 
 urlpatterns = [
 	path('', views.ProductListView.as_view(), name = 'product-list'),
+    path('<int:pk>/', views.ProductDetailView.as_view(), name = 'product-detail'),
 	
 ]
 
--- alpha/products/views.py
+++ alpha/products/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.view.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Product
 
@@ -12,5 +12,14 @@ class ProductListView(ListView):
 
 	def get_context_data(self, *args, **kwargs):
 		context = super(ProductListView, self).get_context_data(*args, **kwargs)
+		print(context)
+		return context
+
+class ProductDetailView(DetailView):
+	model = Product
+	template_name = 'products/product_detail.html'
+
+	def get_context_data(self, *args, **kwargs):
+		context = super(ProductDetailView, self).get_context_data(*args, **kwargs)
 		print(context)
 		return context
