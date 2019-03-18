2a58d978e433c799accdab14056ff393d0684f98
NilSagor <masum1201@gmail.com>
2019-03-18 21:53:02 +0800

    ProductManager added

--- alpha/products/models.py
+++ alpha/products/models.py
@@ -18,6 +18,13 @@ def upload_image_path(instance, filename):
 		)
 
 
+class ProductManager(models.Manager):
+	def get_by_id(self, id):
+		qs = self.get_queryset().filter(id = id)
+		if qs.count() == 1:
+			return qs.first()
+		return None
+
 
 
 # Create your models here.
